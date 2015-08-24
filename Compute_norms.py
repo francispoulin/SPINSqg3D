c3dda09f46d6e614cf5eb9ec1f3095d38cd041f8
bastorer <bastorer@uwaterloo.ca>
2015-08-24 18:58:20 -0400

    Adding basic diagnostic functions

---
+++ Compute_norms.py
@@ -0,0 +1,85 @@
+import matplotlib
+matplotlib.use('Agg')
+import matpy as mp
+import numpy as np
+import spinspy as spy
+import matplotlib.pyplot as plt
+import matplotlib.animation as anim
+import sys, os, shutil, tempfile
+import subprocess
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import glob
+
+try: # Try using mpi
+    from mpi4py import MPI
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    num_procs = comm.Get_size()
+except:
+    rank = 0
+    num_procs = 1
+
+# Load some information
+dat = spy.get_params()
+Nx = dat.Nx, Ny = dat.Ny, Nz = dat.Nz
+dt = dat.tplot/(24.*3600) 
+ct = len(glob.glob('q.*')) # Number of files of form q.*
+ts = np.arange(0,(ct+1)*dt,dt)
+
+if dat.method == 'linear':
+    qb = np.fromfile('qb','<d').reshape((Nx,Ny,Nz))
+
+norms = np.zeros(ct+1)
+
+cont = True
+ii = rank
+while cont:
+    try:
+        print('Processor {0:d} accessing q.{1:d}'.format(rank,ii))
+
+        var_3d = np.fromfile('q.{0:d}'.format(ii),'<d').reshape((Nx,Ny,Nz))
+        if dat.method == 'linear':
+            var_3d -= qb
+        norms[ii] = np.linalg.norm(var_3d.ravel())
+
+        ii += num_procs # Parallel, so skip a `bunch'
+    except:
+        ii += -num_procs 
+        cont = False
+
+
+# Determine how many outputs there were
+final = np.zeros(num_procs)
+max_is = np.zeros(num_procs)
+final[rank] = ii
+comm.Reduce([final, MPI.DOUBLE], [max_is,MPI.DOUBLE], op=MPI.MAX, root=0)
+max_ii = np.max(max_is)
+
+the_norms = np.zeros(ct+1)
+comm.Reduce([norms, MPI.DOUBLE], [the_norms,MPI.DOUBLE], op=MPI.SUM, root=0)
+
+if rank == 0:
+
+    the_norms = the_norms[:max_ii]
+    ts = ts[:max_ii]
+    Dt = mp.FiniteDiff(ts,4)
+
+    fig = plt.figure()
+    ax  = plt.gca()
+    axt = ax.twinx()
+    ax.plot(ts,the_norms,'b',linewidth=2)
+    ax.set_xlabel('t (days)')
+    ax.set_ylabel('norm(q) (blue)')
+    ax.set_yscale('log')
+
+    deriv = Dt.dot(np.log(the_norms))
+    deriv[np.isinf(deriv)] = np.nan
+    axt.plot(ts,deriv,'r',linewidth=2)
+    axt.set_ylim((0,0.1))
+    axt.set_ylabel('d/dt(log(norm)) (red)')
+
+    plt.draw()
+    fig.set_tight_layout(True)
+
+    np.savez('Diagnostics/perturbation_norms',ts=ts,norms=the_norms)
+    plt.savefig('Diagnostics/perturbation_norms.png')
---
+++ movies_qg.py
@@ -0,0 +1,187 @@
+import matplotlib
+matplotlib.use('Agg')
+import matpy as mp
+import numpy as np
+import spinspy as spy
+import matplotlib.pyplot as plt
+import matplotlib.animation as anim
+import sys, os, shutil, tempfile
+import subprocess
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+try: # Try using mpi
+    from mpi4py import MPI
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    num_procs = comm.Get_size()
+except:
+    rank = 0
+    num_procs = 1
+
+## USER CHANGE THIS SECTION
+out_direct = os.getcwd() + '/Videos'# Where to put the movies
+                                    #   the directory needs to already exist!
+the_name = 'QG_'          # What to call the movies
+                                    #   the variable name will be appended
+out_suffix = 'mp4'                  # Movie type
+mov_fps = 10                        # Framerate for movie
+plt_var = ['q']                     # Which variables to plot
+cmap = 'darkjet'
+##
+
+# Load some information
+dat = spy.get_params()
+Nx = dat.Nx
+Ny = dat.Ny
+Nz = dat.Nz
+tplot = dat.tplot
+
+x = np.fromfile('xgrid','<d').reshape((Nx,Ny,Nz))[:,0,0]/1e3
+y = np.fromfile('ygrid','<d').reshape((Nx,Ny,Nz))[0,:,0]/1e3
+z = np.fromfile('zgrid','<d').reshape((Nx,Ny,Nz))[0,0,:]/1e3
+
+dx = x[1]-x[0]
+dy = y[1]-y[0]
+dz = z[1]-z[0]
+
+gridx = np.zeros(Nx+1)
+gridx[1:] = x+dx/2
+gridx[0] = x[0] - dx/2
+
+gridy = np.zeros(Ny+1)
+gridy[1:] = y+dy/2
+gridy[0] = y[0] - dy/2
+
+gridz = np.zeros(Nz+1)
+gridz[1:] = z+dz/2
+gridz[0] = z[0] - dz/2
+
+# Prepare directories
+if rank == 0:
+    print('Video files will be saved in {0:s}'.format(out_direct))
+    tmp_dir = tempfile.mkdtemp(dir=out_direct)
+    fig_prefix = tmp_dir + '/' + the_name # path for saving frames
+    out_prefix = out_direct + '/' + the_name # path for saving videos
+    for proc in range(1,num_procs):
+        comm.send(tmp_dir,dest=proc,tag=1)
+        comm.send(fig_prefix,dest=proc,tag=2)
+        comm.send(out_prefix,dest=proc,tag=3)
+else:
+    tmp_dir = comm.recv(source=0,tag=1)
+    fig_prefix = comm.recv(source=0,tag=2)
+    out_prefix = comm.recv(source=0,tag=3)
+
+# Initialize the meshes
+fig_xy = plt.figure(figsize=(6,5))
+fig_xy_ttl = fig.suptitle('')
+QM_xy = plt.pcolormesh(gridx,gridy,np.zeros((Ny,Nx)),cmap=cmap)
+plt.axis('tight')
+cbar = plt.colorbar()
+
+fig_xz = plt.figure(figsize=(6,5))
+fig_xz_ttl = fig.suptitle('')
+QM_xz = plt.pcolormesh(gridx,gridz,np.zeros((Nz,Nx)),cmap=cmap)
+plt.axis('tight')
+cbar = plt.colorbar()
+
+fig_xy_p = plt.figure(figsize=(6,5))
+fig_xy_p_ttl = fig.suptitle('')
+QM_xy_p = plt.pcolormesh(gridx,gridy,np.zeros((Ny,Nx)),cmap=cmap)
+plt.axis('tight')
+cbar = plt.colorbar()
+
+fig_xz_p = plt.figure(figsize=(6,5))
+fig_xz_p_ttl = fig.suptitle('')
+QM_xz_p = plt.pcolormesh(gridx,gridz,np.zeros((Nz,Nx)),cmap=cmap)
+plt.axis('tight')
+cbar = plt.colorbar()
+
+ii = rank # parallel, so start where necessary
+cont = True
+
+# Load background state
+bg = np.fromfile(dat.qg_file,'<d').reshape((Nx,Ny,Nz))
+
+while cont:
+    try:
+        var_3d = np.fromfile('q.{0:d}'.format(ii),'<d').reshape((Nx,Ny,Nz))
+        print('Processor {0:d} accessing q.{1:d}'.format(rank,ii))
+
+        if method == 'linear':
+            QM_xy_p.set_array(var_3d[:,:,Nz/2].T.ravel())
+            QM_xz_p.set_array(var_3d[:,Ny/2,:].T.ravel())
+            QM_xy.set_array((var_3d+bg)[:,:,Nz/2].T.ravel())
+            QM_xz.set_array((var_3d+bg)[:,Ny/2,:].T.ravel())
+
+            cv = np.max(abs(var_3d[:,:,Nz/2].ravel()))
+            QM_xy_p.set_clim((-cv,cv))
+            cv = np.max(abs(var_3d[:,Ny/2,:].ravel()))
+            QM_xz_p.set_clim((-cv,cv))
+            cv = np.max(abs((var_3d+bg)[:,:,Nz/2].ravel()))
+            QM_xy.set_clim((-cv,cv))
+            cv = np.max(abs((var_3d+bg)[:,:,Nz/2].ravel()))
+            QM_xz.set_clim((-cv,cv))
+        elif method == 'nonlinear':
+            QM_xy.set_array(var_3d[:,:,Nz/2].T.ravel())
+            QM_xz.set_array(var_3d[:,Ny/2,:].T.ravel())
+            QM_xy_p.set_array((var_3d-bg)[:,:,Nz/2].T.ravel())
+            QM_xz_p.set_array((var_3d-bg)[:,Ny/2,:].T.ravel())
+
+            cv = np.max(abs(var_3d[:,:,Nz/2].ravel()))
+            QM_xy.set_clim((-cv,cv))
+            cv = np.max(abs(var_3d[:,Ny/2,:].ravel()))
+            QM_xz.set_clim((-cv,cv))
+            cv = np.max(abs((var_3d-bg)[:,:,Nz/2].ravel()))
+            QM_xy_p.set_clim((-cv,cv))
+            cv = np.max(abs((var_3d-bg)[:,:,Nz/2].ravel()))
+            QM_xz_p.set_clim((-cv,cv))
+
+        QM_xy.changed()
+        QM_xz.changed()
+        QM_xy_p.changed()
+        QM_xz_p.changed()
+
+        fig_xy_ttl.set_text('q (full) : t = {0:.3g}'.format(ii*tplot))
+        fig_xz_ttl.set_text('q (full) : t = {0:.3g}'.format(ii*tplot))
+        fig_xy_p_ttl.set_text('q (pert) : t = {0:.3g}'.format(ii*tplot))
+        fig_xz_p_ttl.set_text('q (pert) : t = {0:.3g}'.format(ii*tplot))
+
+        plt.draw()
+
+        fig_xy.savefig('{0:s}q-{1:05d}_xy.png'.format(fig_prefix,ii))
+        fig_xz.savefig('{0:s}q-{1:05d}_xz.png'.format(fig_prefix,ii))
+        fig_xy_p.savefig('{0:s}q-{1:05d}_xy_p.png'.format(fig_prefix,ii))
+        fig_xz_p.savefig('{0:s}q-{1:05d}_xz_p.png'.format(fig_prefix,ii))
+        
+        ii += num_procs # Parallel, so skip a `bunch'
+    except:
+        cont = False
+
+# Have processor 0 wait for the others
+if num_procs > 1:
+    if rank > 0:
+        isdone = True
+        comm.send(isdone, dest=0, tag=rank)
+        print('Processor {0:d} done.'.format(rank))
+    elif rank == 0:
+        isdone = False
+        for proc in range(1,num_procs):
+            isdone = comm.recv(source=proc,tag=proc)
+
+# Now that the individual files have been written, we need to parse them into a movie.
+if rank == 0:
+
+    # Make the videos
+    for plot_type in ['xy','xz','xy_p','xz_p']:
+        in_name = '{0:s}q-%05d_{1:s}.png'.format(fig_prefix,plot_type)
+        out_name = '{0:s}_{1:s}.{2:s}'.format(out_prefix,plot_type,out_suffix)
+        cmd = ['ffmpeg', '-framerate', str(mov_fps), '-r', str(mov_fps),
+            '-i', in_name, '-y', '-q', '1', '-pix_fmt', 'yuv420p', out_name]
+        subprocess.call(cmd)
+
+    print('--------')
+    print('Deleting directory of intermediate frames.')
+    shutil.rmtree(tmp_dir)
+    print('Video creation complete.')
+    print('Processor {0:d} done.'.format(rank))
+
